d63a97fb8e9333b18d4b98e3e16770eff21146c2
Veerababu <veerababu@Veerababus-MacBook-Air.local>
2021-03-09 17:43:56 +0530

    first commit

---
+++ lib/rails_jwt_auth-master/app/controllers/concerns/rails_jwt_auth/render_helper.rb
@@ -0,0 +1,43 @@
+module RailsJwtAuth
+  module RenderHelper
+    def render_session(jwt, user)
+      auth_field = RailsJwtAuth.auth_field_name
+      render json: {session: {jwt: jwt, auth_field => user[auth_field]}}, status: 201
+    end
+
+    def render_registration(resource)
+      render json: "User Successfully Created", root: true, status: 201
+    end
+
+    def render_profile(resource)
+      render json: resource, root: true, status: 200
+    end
+
+    def render_222
+      render plain: "Confirmation Email Has been to the User Email"
+    end
+
+
+
+    def render_223
+      render json:"Email Confirmation Done"
+    end
+
+
+    def render_204
+      head 204
+    end
+
+    def render_404
+      head 404
+    end
+
+    def render_410
+      head 410
+    end
+
+    def render_422(errors)
+      render json: {errors: errors}, status: 422
+    end
+  end
+end
